26b37d2d7bc63b6bfa39ceef3d30047dddf9014e
sm-mosavian <senexen@gmail.com>
2021-04-07 08:56:08 +0430

    Initial commit

---
+++ src/main/java/ir/sm/weblog/WeblogApplication.java
@@ -0,0 +1,13 @@
+package ir.sm.weblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogApplication.class, args);
+    }
+
+}
