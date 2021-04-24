74bba4244d25a649e4afb6a2b65cced6a1f30657
sm-mosavian <senexen@gmail.com>
2021-04-24 09:19:25 +0430

    commit

---
+++ src/main/java/ir/sm/weblog/config/SpringSecurityConfig.java
@@ -0,0 +1,10 @@
+package ir.sm.weblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+}
